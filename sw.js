0df8d76a2c67da7d4139f3f55a5d8af16ea1265c
VFCK2 <9689fan@gmail.com>
2026-09-13 13:20:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'o-sora-v10';
+const CACHE = 'o-sora-v12';
 const CORE = ['./', './index.html', './manifest.json', './apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
